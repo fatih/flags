9e27e9cd9b61af25807113ebd86689124815d694
Fatih Arslan <ftharsln@gmail.com>
2015-06-05 17:09:29 +0300

    values: change the IntSlice is created and initialized

--- values.go
+++ values.go
@@ -8,10 +8,11 @@ import (
 
 type StringSliceValue []string
 
-// New returns a new StringList satisfies the flag.Value interface. This is
+// NewStringSlice returns a new StringSlice which satisfies the flag.Value interface. This is
 // useful to be used with flag.FlagSet. For the global flag instance,
 // StringSlice() and StringSliceVar() can be used.
 func NewStringSlice(val []string, p *[]string) *StringSliceValue {
+	*p = val
 	return (*StringSliceValue)(p)
 }
 
@@ -45,21 +46,17 @@ func StringSliceVar(p *[]string, value []string, name, usage string) {
 	flag.Var(NewStringSlice(value, p), name, usage)
 }
 
-// IntList is an implementation of flag.Value interface that accepts a comma
-// separated list of integers and populates a []int slice.
-//
-// Example:
-//
-//  var ids []int
-//  flag.Var(flags.IntListVar(&ids), "server", "IDs to be used")
-type IntList []int
+type IntSliceValue []int
 
-// New returns a new *IntList.
-func IntListVar(p *[]int) *IntList {
-	return (*IntList)(p)
+// NewIntSlice returns a new IntSlice which satisfies the flag.Value interface.
+// This is useful to be used with flag.FlagSet. For the global flag instance,
+// IntSlice() and IntSliceVar() can be used.
+func NewIntSlice(val []int, p *[]int) *IntSliceValue {
+	*p = val
+	return (*IntSliceValue)(p)
 }
 
-func (i *IntList) Set(val string) error {
+func (i *IntSliceValue) Set(val string) error {
 	// if empty default is used
 	if val == "" {
 		return nil
@@ -75,16 +72,32 @@ func (i *IntList) Set(val string) error {
 		list = append(list, i)
 	}
 
-	*i = IntList(list)
+	*i = IntSliceValue(list)
 	return nil
 }
 
-func (i *IntList) Get() interface{} { return []int(*i) }
+func (i *IntSliceValue) Get() interface{} { return []int(*i) }
 
-func (i *IntList) String() string {
+func (i *IntSliceValue) String() string {
 	var list []string
 	for _, in := range *i {
 		list = append(list, strconv.Itoa(in))
 	}
 	return strings.Join(list, ",")
+}
+
+// IntSlice defines a []int flag with specified name, default value, and usage
+// string. The return value is the address of a []int variable that stores
+// the value of the flag.
+func IntSlice(value []int, name, usage string) *[]int {
+	p := new([]int)
+	flag.Var(NewIntSlice(value, p), name, usage)
+	return p
+}
+
+// IntSliceVar defines a []int flag with specified name, default value, and
+// usage string. The argument p points to a []int variable in which to store
+// the value of the flag.
+func IntSliceVar(p *[]int, value []int, name, usage string) {
+	flag.Var(NewIntSlice(value, p), name, usage)
 }
--- values_test.go
+++ values_test.go
@@ -46,10 +46,35 @@ func TestStringListFlagSet(t *testing.T) {
 }
 
 func TestIntList(t *testing.T) {
+	ids := IntSlice(nil, "ids", "Servers to be used")
+
+	os.Args = []string{"images", "-ids", "123,456"}
+	flag.Parse()
+
+	want := []int{123, 456}
+	if !reflect.DeepEqual(*ids, want) {
+		t.Errorf("Ids = %q, want %q", *ids, want)
+	}
+}
+
+func TestIntListVar(t *testing.T) {
+	var ids []int
+	IntSliceVar(&ids, nil, "idss", "Servers to be used")
+
+	os.Args = []string{"images", "-idss", "123,456"}
+	flag.Parse()
+
+	want := []int{123, 456}
+	if !reflect.DeepEqual(ids, want) {
+		t.Errorf("Ids = %q, want %q", ids, want)
+	}
+}
+
+func TestIntListFlagSet(t *testing.T) {
 	f := flag.NewFlagSet("TestTags", flag.PanicOnError)
 
 	var ids []int
-	f.Var(IntListVar(&ids), "ids", "Ids to be used")
+	f.Var(NewIntSlice(nil, &ids), "ids", "Ids to be used")
 	f.Parse([]string{"-ids", "123,456"})
 
 	want := []int{123, 456}
